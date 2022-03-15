d095f4318703f68b1aa96ffb10d11c7cbe010408
subhash <subhashs0620@gmail.com>
2022-03-15 11:34:41 +0530

    update contact buttons

--- src/Common/SocialButtons.js
+++ src/Common/SocialButtons.js
@@ -7,20 +7,22 @@ export const SocialButtons = ({props}) => {
         <div className="social-buttons flex justify-content-evenly">
             <span className="pl-1">
                 <Button icon="pi pi-linkedin" target="_blank"
+                        className={classNames(props.theme === 'light' ? "bg-blue-600" : "bg-blue-300", "border-0")}
                         onClick={() => window.open('https://www.linkedin.com/in/subhashs0620/', '_blank')}/>
             </span>
             <span className="pl-1">
                 <Button icon="pi pi-instagram"
-                        className="bg-pink-500 border-pink-500"
+                        className="bg-pink-500 border-0"
                         onClick={() => window.open('https://www.instagram.com/subhash_011/', '_blank')}/>
             </span>
             <span className="pl-1 p-button-lg">
                 <Button icon="pi pi-discord"
-                        className={classNames(props.theme === 'light' ? "bg-gray-800 border-gray-800" : "bg-gray-300 border-gray-300", "border-0")}
+                        className={classNames(props.theme === 'light' ? "bg-gray-800" : "bg-gray-300", "border-0")}
                         onClick={() => window.open('https://discord.com/users/subhash011#0822', '_blank')}/>
             </span>
             <span className="pl-1">
                 <Button icon="pi pi-twitter"
+                        className={classNames(props.theme === 'light' ? "bg-blue-600" : "bg-blue-300", "border-0")}
                         onClick={() => window.open('https://twitter.com/subhash_011', '_blank')}/>
             </span>
         </div>
