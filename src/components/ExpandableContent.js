0291aa40562422c6679df756c4aede76ec5c31f7
subhash <subhashs0620@gmail.com>
2022-03-18 19:36:02 +0530

    fix expanding card

--- src/components/ExpandableContent.js
+++ src/components/ExpandableContent.js
@@ -1,42 +1,25 @@
-import React, {useEffect, useRef, useState} from "react";
-import {useWindowSize} from "../utils";
-import {animated, useSpring} from "react-spring";
 import {Button} from "primereact/button";
+import React, {useState} from "react";
+import useCollapse from "react-collapsed";
 
 const ExpandableContent = ({item}) => {
-    const [openItem, setOpenItem] = useState(null);
-    const windowSize = useWindowSize();
-    const ref = useRef(null);
-    const [style, animateStyle] = useSpring(() => ({
-        height: '0px'
-    }), []);
-
-    useEffect(() => {
-        animateStyle.start({
-            height: (openItem ? ref.current.offsetHeight + openItem.additionalHeight : 0) + 'px',
-        });
-    }, [ref, openItem, animateStyle, windowSize]);
-
-    useEffect(() => {
-        if (item.expanded) {
-            setOpenItem(item);
-        }
-    }, [item]);
+    const [openItem, setOpenItem] = useState(item.expanded ? item : null);
+    const {getCollapseProps, getToggleProps} = useCollapse({
+        defaultExpanded: true,
+        isExpanded: !!openItem
+    })
 
     return (
         <React.Fragment>
-            {item.extra && <div style={{height: '100%'}}>
-                <animated.div style={{overflow: "hidden", ...style}}>
-                    <div ref={ref}>
-                        {item.extraContent}
-                    </div>
-                </animated.div>
+            {item.extra && <div className="pt-2" {...getCollapseProps()}>
+                {item.extraContent}
             </div>}
-            {item.extra && <div>
+            {item.extra && <div className="pt-2">
                     <span className="w-full flex justify-content-end">
-                        <Button label={`See ${openItem ? 'less' : 'more'}`} className="p-button-text"
+                        <Button {...getToggleProps()} label={`See ${openItem ? 'less' : 'more'}`}
+                                className="p-button-text"
                                 onClick={() => {
-                                    setOpenItem(!openItem ? item : 0);
+                                    setOpenItem(!!!openItem ? item : null);
                                 }}/>
                     </span>
             </div>}
