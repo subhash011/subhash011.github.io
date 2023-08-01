6eb0354b8f698d10963095dd02c32975f512b2df
Subhash S <subhashs0620@gmail.com>
2023-08-01 12:15:46 +0000

    update photo

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import {useFormik} from 'formik';
 import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
+import { BlockUI } from 'primereact/blockui';
 import {classNames} from 'primereact/utils';
 import {Toast} from 'primereact/toast';
 import {InputTextarea} from "primereact/inputtextarea";
@@ -79,65 +80,62 @@ function ContactForm() {
     const getFormErrorMessage = (name) => {
         return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
     };
-    if (loading) {
-        return (
-            <div className="flex justify-content-center align-items-center">
-                <ProgressSpinner style={{width: '50px', height: '50px'}} strokeWidth="8" fill="var(--surface-ground)"/>
-            </div>
-        );
-    }
     return (
         <React.Fragment>
             <Toast ref={toast} position="top-right"/>
-            <div className="card flex justify-content-center align-items-center">
-                <form className="formgrid grid" onSubmit={formik.handleSubmit}>
-                    <div className="field col-12 lg:col-6 mt-4">
+            <BlockUI template={<div className="absolute flex justify-content-center align-items-center col-12 z-5">
+                                <ProgressSpinner className="absolute" style={{width: '50px', height: '50px'}} strokeWidth="8" />
+                               </div>} blocked={loading}>
+                <div className="card flex justify-content-center align-items-center">
+                    <form className="p-2 formgrid grid" onSubmit={formik.handleSubmit}>
+                        <div className="field col-12 lg:col-6 mt-4">
+                            <span className="p-float-label p-input-icon-right w-full">
+                                    <i className="pi pi-user"/>
+                                    <InputText id="name" name="name" value={formik.values.name}
+                                            onChange={formik.handleChange}
+                                            className={classNames({'p-invalid': isFormFieldValid('name')}, 'w-full')}/>
+                                    <label htmlFor="name"
+                                        className={classNames({'p-error': isFormFieldValid('name')}, 'w-full')}>Name*</label>
+                                </span>
+                            {getFormErrorMessage('name')}
+                        </div>
+                        <div className="field col-12 lg:col-6 mt-4">
                         <span className="p-float-label p-input-icon-right w-full">
-                                <i className="pi pi-user"/>
-                                <InputText id="name" name="name" value={formik.values.name}
-                                           onChange={formik.handleChange}
-                                           className={classNames({'p-invalid': isFormFieldValid('name')}, 'w-full')}/>
-                                <label htmlFor="name"
-                                       className={classNames({'p-error': isFormFieldValid('name')}, 'w-full')}>Name*</label>
+                                    <i className="pi pi-envelope"/>
+                                    <InputText id="email" name="email" value={formik.values.email}
+                                            onChange={formik.handleChange}
+                                            className={classNames({'p-invalid': isFormFieldValid('email')}, 'w-full')}/>
+                                    <label htmlFor="email"
+                                        className={classNames({'p-error': isFormFieldValid('email')}, 'w-full')}>Email*</label>
+                                </span>
+                            {getFormErrorMessage('email')}
+                        </div>
+                        <div className="field col-12 mt-4">
+                        <span className="p-float-label p-input-icon-right w-full">
+                                    <InputText id="subject" name="subject" value={formik.values.subject}
+                                            onChange={formik.handleChange}
+                                            className={classNames({'p-invalid': isFormFieldValid('subject')}, 'w-full')}/>
+                                    <label htmlFor="subject"
+                                        className={classNames({'p-error': isFormFieldValid('subject')}, 'w-full')}>Subject</label>
+                                </span>
+                            {getFormErrorMessage('subject')}
+                        </div>
+                        <div className="field col-12 mt-4">
+                        <span className="p-float-label p-input-icon-right w-full">
+                            <InputTextarea rows={5} id="message" name="message" value={formik.values.message}
+                                            onChange={formik.handleChange}
+                                            className={classNames({'p-invalid': isFormFieldValid('message')}, 'w-full')}/>
+                            <label htmlFor="message"
+                                    className={classNames({'p-error': isFormFieldValid('message')}, 'w-full')}>Message*</label>
                             </span>
-                        {getFormErrorMessage('name')}
-                    </div>
-                    <div className="field col-12 lg:col-6 mt-4">
-                       <span className="p-float-label p-input-icon-right w-full">
-                                <i className="pi pi-envelope"/>
-                                <InputText id="email" name="email" value={formik.values.email}
-                                           onChange={formik.handleChange}
-                                           className={classNames({'p-invalid': isFormFieldValid('email')}, 'w-full')}/>
-                                <label htmlFor="email"
-                                       className={classNames({'p-error': isFormFieldValid('email')}, 'w-full')}>Email*</label>
-                            </span>
-                        {getFormErrorMessage('email')}
-                    </div>
-                    <div className="field col-12 mt-4">
-                       <span className="p-float-label p-input-icon-right w-full">
-                                <InputText id="subject" name="subject" value={formik.values.subject}
-                                           onChange={formik.handleChange}
-                                           className={classNames({'p-invalid': isFormFieldValid('subject')}, 'w-full')}/>
-                                <label htmlFor="subject"
-                                       className={classNames({'p-error': isFormFieldValid('subject')}, 'w-full')}>Subject</label>
-                            </span>
-                        {getFormErrorMessage('subject')}
-                    </div>
-                    <div className="field col-12 mt-4">
-                       <span className="p-float-label p-input-icon-right w-full">
-                           <InputTextarea rows={5} id="message" name="message" value={formik.values.message}
-                                          onChange={formik.handleChange}
-                                          className={classNames({'p-invalid': isFormFieldValid('message')}, 'w-full')}/>
-                           <label htmlFor="message"
-                                  className={classNames({'p-error': isFormFieldValid('message')}, 'w-full')}>Message*</label>
-                        </span>
-                        {getFormErrorMessage('message')}
-                    </div>
-                    <div className="field col-12 mt-4 flex justify-content-center">
-                        <Button label="Submit" className="p-button lg:w-25rem w-full" type="submit"/>
-                    </div>
-                </form>
-            </div>
+                            {getFormErrorMessage('message')}
+                        </div>
+                        <div className="field col-12 mt-4 flex justify-content-center">
+                            <Button label="Submit" className="p-button lg:w-25rem w-full" type="submit"/>
+                        </div>
+                    </form>
+                </div>
+            </BlockUI>
         </React.Fragment>
     );
 }
